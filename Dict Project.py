b556d614571ba2adf9b5de536e0b6ca8ff42bd5a
rom5rays <rom5rays@gmail.com>
2024-12-12 13:10:39 +0800

    Dict Project

---
+++ Dict Project.py
@@ -0,0 +1,15 @@
+test_dict = {'Codingal': 2, 'is': 2, 'best': 2, 'for': 2, 'Coding': 1}
+
+print("The original dictionary:" + str(test_dict))
+
+k = int(input('Enter a number for the value of k:'))
+
+res = 0
+
+for key in test_dict:
+    if test_dict[key] == k:
+        res += 1
+
+print(res)        
+print("Frequency of k is:" + str(res))
+print(f"Frequency of k is: {res}")
